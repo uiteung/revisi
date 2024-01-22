d667c1189a83ccccd1b8249b77b0ae0e82eec7ed
rofinafiin <rofinafiisr@gmail.com>
2024-01-22 08:00:46 +0000

    perbaikan url

--- js/croot.js
+++ js/croot.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
   
       console.log(data);
   
-      postWithToken("https://kimteungbim.ulbi.ac.id/public/revisi/", "LOGIN", token,  data, (results) => {
+      postWithToken("https://kimteungbim.ulbi.ac.id/revisi/", "LOGIN", token,  data, (results) => {
         // Handle results for the second action
         console.log(results);
       });
